79668a3d3c45e7d878e5b75e2067600ade92c216
JeremyBon <44227843+JeremyBon@users.noreply.github.com>
2025-12-12 07:52:37 +0100

    Test/ci (#10)
    * 💚 fix: semantic release
    
    * fix: 💚 semantic release
    
    ---------
    
    Co-authored-by: Jeremy Bon <jeremy_bon@carrefour.com>

--- front/commitlint.config.js
+++ front/commitlint.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  parserPreset: {
-    parserOpts: {
-      // Regex qui accepte un emoji optionnel au début
-      headerPattern: /^(?:.{0,2}\s)?(\w*)(?:\((.+)\))?!?:\s(.+)$/,
-      headerCorrespondence: ["type", "scope", "subject"],
-    },
-  },
 };
